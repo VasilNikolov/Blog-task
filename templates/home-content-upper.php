ae5a5a129e52cc6fd8072ddc3f2d3578e1e81032
VasilNikolov <vasil97@yahoo.com>
2015-12-28 19:16:32 +0200

    home-content-upper.php half complete!

---
+++ templates/home-content-upper.php
@@ -0,0 +1,11 @@
+<div class="container-upper clearfix">
+    <div class="content-first-row first"></div>
+    <div class="content-first-row second"></div>
+    <div class="content-first-row third"></div>
+    <div class="content-second-row largest"></div>
+    <div class="content-second-row medium"></div>
+    <div class="content-second-row responsive">RESPONSIVE</div>
+    <div class="content-third-row latest-news">LATEST NEWS</div>
+    <div class="content-third-row eighth"></div>
+    <div class="content-third-row last"></div>
+</div>
