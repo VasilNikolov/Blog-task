0689ee11a9e9832c8b1b8c6634a063d4fb33e72f
stoyan-yanchev <stoian19@gmail.com>
2015-12-29 19:07:58 +0200

    content

---
+++ templates/home-content.php
@@ -0,0 +1,153 @@
+<div class="wrapper">
+	<h2>A Theme Unlike Any Other. Simply Fantastic!</h2>
+	<nav class="content-nav">
+		<ul>
+			<li><a class="active" href="">all</a></li>
+			<li><a href="">news</a></li>
+			<li><a href="">design</a></li>
+			<li><a href="">print</a></li>
+			<li><a href="">art</a></li>
+			<li><a href="">development</a></li>
+		</ul>
+	</nav>
+	<article class="posts-wrapper clearfix">
+		<div class="post-column">
+			<div class="post">
+				<div class="content-image">
+					<img src="img/sticker-mule.jpg" alt="sticker-mule">
+					<div class="image-hover"></div>
+				</div>
+				<div class="content">
+					<h3>Sticker Mule</h3>
+					<blockquote>June 15, 2012 / news, contests</blockquote>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, odio voluptates esse dolorem laboriosam eum provident aliquam cum totam odit.</p>
+					<a class="more-button" href="">More</a>
+				</div>
+			</div>
+			<div class="post">
+				<div class="content-image">
+					<img src="img/big-bick-bunny.jpg" alt="big-bick-bunny">
+					<div class="image-hover"></div>
+				</div>
+				<div class="content">
+					<h3>Big Buck Bunny</h3>
+					<blockquote>June 15, 2012</blockquote>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, odio voluptates esse dolorem laboriosam eum provident aliquam cum totam odit.</p>
+					<a class="more-button" href="">More</a>
+				</div>
+			</div>
+			<div class="post">
+				<img src="img/pinterest-icons.jpg" alt="pinterest-icons">
+				<div class="content">
+					<h3>Pinterest Icons</h3>
+					<blockquote>June 15, 2012</blockquote>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, odio voluptates esse dolorem laboriosam eum provident aliquam cum totam odit.</p>
+					<a class="more-button" href="">More</a>
+				</div>
+			</div>
+		</div>
+		<div class="post-column">
+			<div class="post">
+				<img src="img/10-amazing-websites.jpg" alt="10-amazing-websites">
+				<div class="content">
+					<h3>10 Amazing Websites</h3>
+					<blockquote>June 15, 2012</blockquote>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, odio voluptates esse dolorem laboriosam eum provident aliquam cum totam odit.</p>
+					<a class="more-button" href="">More</a>
+				</div>
+			</div>
+			<div class="post">
+				<img src="img/character-design.jpg" alt="character-design">
+				<div class="content">
+					<h3>Character Design</h3>
+					<blockquote>June 15, 2012</blockquote>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, odio voluptates esse dolorem laboriosam eum provident aliquam cum totam odit.</p>
+					<a class="more-button" href="">More</a>
+				</div>
+			</div>
+			<div class="post">
+				<img src="img/ipad-3-review.jpg" alt="ipad-3-review">
+				<div class="content">
+					<h3>iPad 3 Review</h3>
+					<blockquote>June 15, 2012</blockquote>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, odio voluptates esse dolorem laboriosam eum provident aliquam cum totam odit.</p>
+					<a class="more-button" href="">More</a>
+				</div>
+			</div>
+		</div>
+		<div class="post-column">
+			<div class="post">
+				<img src="img/top-iphone-apps.jpg" alt="top-iphone-apps">
+				<div class="content">
+					<h3>Top iPhone Apps</h3>
+					<blockquote>June 15, 2012</blockquote>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, odio voluptates esse dolorem laboriosam eum provident aliquam cum totam odit.</p>
+					<a class="more-button" href="">More</a>
+				</div>
+			</div>
+			<div class="post">
+				<img src="img/service-icons.jpg" alt="service-icons">
+				<div class="content">
+					<h3>Service Icons</h3>
+					<blockquote>June 15, 2012</blockquote>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, odio voluptates esse dolorem laboriosam eum provident aliquam cum totam odit.</p>
+					<a class="more-button" href="">More</a>
+				</div>
+			</div>
+			<div class="post">
+				<img src="img/social-media-buttons.jpg" alt="social-media-buttons">
+				<div class="content">
+					<h3>Social Media Buttons</h3>
+					<blockquote>June 15, 2012</blockquote>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, odio voluptates esse dolorem laboriosam eum provident aliquam cum totam odit.</p>
+					<a class="more-button" href="">More</a>
+				</div>
+			</div>
+		</div>
+		<div class="post-column">
+			<div class="post">
+				<img src="img/photo-shoot.jpg" alt="photo-shoot">
+				<div class="content">
+					<h3>Photo Shoot</h3>
+					<blockquote>June 15, 2012</blockquote>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, odio voluptates esse dolorem laboriosam eum provident aliquam cum totam odit.</p>
+					<a class="more-button" href="">More</a>
+				</div>
+			</div>
+			<div class="post">
+				<img src="img/wedding-card.jpg" alt="wedding-card">
+				<div class="content">
+					<h3>Wedding Card</h3>
+					<blockquote>June 15, 2012</blockquote>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, odio voluptates esse dolorem laboriosam eum provident aliquam cum totam odit.</p>
+					<a class="more-button" href="">More</a>
+				</div>
+			</div>
+			<div class="post">
+				<img src="img/silver-ul-kit.jpg" alt="silver-ul-kit">
+				<div class="content">
+					<h3>Silver Ul Kit</h3>
+					<blockquote>June 15, 2012</blockquote>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, odio voluptates esse dolorem laboriosam eum provident aliquam cum totam odit.</p>
+					<a class="more-button" href="">More</a>
+				</div>
+			</div>
+		</div>
+	</article>
+	<section class="page-buttons">
+		<ul>
+			<li class="page-button"><a href="">1</a></li>
+			<li class="page-button"><a href="">2</a></li>
+			<li class="page-button"><a href="">3</a></li>
+			<li class="page-button"><a href="">4</a></li>
+		</ul>
+	</section>
+</div>
+
+
+
+
+
+
+
+
